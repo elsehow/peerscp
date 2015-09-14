a40fbdfef8da79b24271ef5c1ee6e4f74b74fc9e
elsehow <yes@cosmopol.is>
2015-09-14 00:08:15 -0700

    stupid simple peer ssh, no security

--- cmd.js
+++ cmd.js
@@ -2,17 +2,19 @@
 
 var Peer = require('simple-peer');
 var exec = require('child_process').exec;
-var split = require('split2');
-var through = require('through2');
 var fs = require('fs');
 var path = require('path');
 
+var signalhub = 'http://indra.webfactional.com'
+var socket = require('socket.io-client')(signalhub)
+var jsonClient = require('request-json').createClient(signalhub)
+
 var minimist = require('minimist');
 var argv = minimist(process.argv.slice(2), {
-    boolean: [ 'listen' ],
+    boolean: [ 'initiate' ],
     alias: {
-        l: 'listen',
-        x: [ 'exec', 'execute' ],
+        i: 'initiate',
+        k: 'key',
         h: 'help'
     }
 });
@@ -22,34 +24,61 @@ if (argv.help || argv._[0] === 'help') {
     ;
 }
 
-var peer = new Peer({
-    initiator: argv.listen,
-    wrtc: require('wrtc'),
-    trickle: false
-});
+var makePeer = function (isInitiator) {
+  return new Peer({
+      initiator: isInitiator,
+      wrtc: require('wrtc'),
+      trickle: false
+  })
+}
 
-var sp = split();
-sp.pipe(through(function (buf, enc, next) {
-    var line = buf.toString('utf8');
-    var intro = /^{/.test(line)
-        ? JSON.parse(line)
-        : JSON.parse(Buffer(line, 'base64'))
-    ;
-    peer.signal(intro);
-    process.stdin.unpipe(sp);
+var handshake = function (reference, type) {
+  return {
+    type: type,
+    reference: reference
+  }
+}
+
+var shareSignal = function (signal, type) {
+  var ref = JSON.stringify(signal)
+  jsonClient.post('/', handshake(ref, type), function (_, _, _) { return })
+  //console.log('shared', signal)
+}
+
+if (!argv.key) {
+  console.log("You'll need a key. Run with --help for more information.")
+  process.exit(0)
+}
+
+var offer_event = "pssh-offer-"+argv.key
+var answer_event = "pssh-answer-"+argv.key
+
+if (argv.initiate) { 
+  socket.on(answer_event, function (d) {
+    socket.disconnect()
+    //console.log('ok, we should be ready to connect now...', d)
+    peer.signal(JSON.parse(d.reference))
     process.stdin.pipe(peer).pipe(process.stdout);
-}));
-process.stdin.pipe(sp);
+  })
+  var peer = makePeer(true)
+  peer.once('signal', function (s) {
+    shareSignal(s, offer_event)
+  })
+  peer.on('connect', function () {
+    process.stdin.pipe(peer)
+  })
+}
 
-peer.once('signal', function (signal) {
-    var ref = Buffer(JSON.stringify(signal)).toString('base64');
-    if (argv.execute) {
-        var ps = exec(argv.execute);
-        ps.stdout.pipe(process.stdout);
-        ps.stderr.pipe(process.stderr);
-        ps.stdin.end(ref);
-    }
-    else {
-        console.log(ref);
-    }
-});
+if (!argv.initiate) {
+  socket.on(offer_event, function (d) {
+    socket.disconnect()
+    var peer = makePeer(false)
+    peer.signal(JSON.parse(d.reference))
+    peer.once('signal', function (s) {
+      shareSignal(s, answer_event)
+    })
+    peer.on('connect', function () {
+      peer.pipe(process.stdout)
+    })
+  })
+}
--- readme.markdown
+++ readme.markdown
@@ -1,32 +1,17 @@
-# rtcat
+# peerssh
 
-webrtc netcat
+peerssh netcat
 
 # usage
 
-```
-rtcat -l
-
-  Initiate a webrtc connection, printing the base64 introducer blob to stdout.
-  Paste the introducer from the other node as the first line on stdin.
-
-rtcat
-
-  Connect to an initiated webrtc connection.
-  Paste the introducer from the other node as the first line on stdin.
-  After pasting the introducer, another introducer will be printed to stdout for
-  the remote node to use.
-
-After the introductions, stdin is forwarded to the remote connection and data
-from the remote connection goes to stdout.
-```
+see peerssh -h
 
 # install
 
-With [npm](https://npmjs.org), to get the `rtcat` command do:
+With [npm](https://npmjs.org), to get the `peerssh` command do:
 
 ```
-npm install -g rtcat
+npm install -g peerssh
 ```
 
 # license
